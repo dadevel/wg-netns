da967b34d36767d7977f6c9385e170e38e4b5bc5
dadevel <dadevel@disroot.org>
2023-05-29 12:46:19 +0200

    update wg-resolve to work together with wg-netns
    Resolves #18.

--- extras/wg-resolve/wg-resolve.sh
+++ extras/wg-resolve/wg-resolve.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -eu
 
-if ! ping -q -c 1 -W "${WG_TIMEOUT:-5}" "$WG_GATEWAY"; then
+# dependencies: dig, ip, ping and wg
+
+WG_ENDPOINT_DOMAIN="${WG_ENDPOINT%%:*}"
+WG_ENDPOINT_PORT="${WG_ENDPOINT##*:}"
+
+if ! ip netns exec "$WG_NAMESPACE" ping -q -c 1 -W "${WG_TIMEOUT:-5}" "$WG_GATEWAY"; then
     echo 'probe failed, resolving endpoint'
-    wg set "$WG_INTERFACE" peer "$WG_PEER" endpoint "$WG_ENDPOINT"
+    ip netns exec "$WG_NAMESPACE" wg set "$WG_INTERFACE" peer "$WG_PEER" endpoint "$(dig +short "$WG_ENDPOINT_DOMAIN"):$WG_ENDPOINT_PORT"
 fi
