32c02b558c296b824ea0e01eae18ad4126120602
dadevel <dadevel@disroot.org>
2023-05-31 16:35:08 +0200

    wg-resolve: support ipv6 dns lookup

--- extras/wg-resolve/wg-resolve.sh
+++ extras/wg-resolve/wg-resolve.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -eu
 
-# dependencies: dig, ip, ping and wg
+# dependencies: cut, getent, ip, ping and wg
 
 WG_ENDPOINT_DOMAIN="${WG_ENDPOINT%%:*}"
 WG_ENDPOINT_PORT="${WG_ENDPOINT##*:}"
 
 if ! ip netns exec "$WG_NAMESPACE" ping -q -c 1 -W "${WG_TIMEOUT:-5}" "$WG_GATEWAY"; then
     echo 'probe failed, resolving endpoint'
-    ip netns exec "$WG_NAMESPACE" wg set "$WG_INTERFACE" peer "$WG_PEER" endpoint "$(dig +short "$WG_ENDPOINT_DOMAIN"):$WG_ENDPOINT_PORT"
+    ip netns exec "$WG_NAMESPACE" wg set "$WG_INTERFACE" peer "$WG_PEER" endpoint "$(getent hosts -- "$WG_ENDPOINT_DOMAIN" | cut -d ' ' -f 1):$WG_ENDPOINT_PORT"
 fi
